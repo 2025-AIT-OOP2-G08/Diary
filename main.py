51f1128b4cd64aed7b6ad0538865eba4d3a7ef7c
reiya8787 <reiyagamer@gmail.com>
2025-10-16 15:45:31 +0900

    池田日記

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from diaries.DiarySample import DiarySample
 from diaries.KaiseiDiary import KaiseiDiary
+from diaries.IkedaDiary import IkedaDiary
 
 # ↓のリストには、メンバーの各自気が格納されます。
 diaries = [DiarySample(), 
            KaiseiDiary(),
+           IkedaDiary(),
            ]
 
 for d in diaries:
